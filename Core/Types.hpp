167f617e21d2e3835624127eb4fdbc1b6c07b678
verodeving <verodeving@gmail.com>
2022-11-23 03:40:49 +0100

    added types

---
+++ Core/Types.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#if lucy_major_version == 0 and lucy_middle_version == 0 and lucy_minor_version == 1
+
+namespace Lucy
+{
+    using u8 = unsigned char;
+
+    using s8 = signed char;
+
+    using u16 = unsigned short;
+
+    using s16 = signed short;
+
+    using u32 = unsigned int;
+
+    using s32 = signed int;
+
+    using f32 = float;
+
+    using u64 = unsigned long long int;
+
+    using s64 = signed long long int;
+
+    using f64 = double;
+}
+
+#endif
